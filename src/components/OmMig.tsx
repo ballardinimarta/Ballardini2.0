542fbfdee42a022221f9dc5afbcf1722a0855bb2
ballardinimarta <marta.ballardini@gmail.com>
2021-10-19 13:57:32 +0200

    update aboutme and skills

--- src/components/OmMig.tsx
+++ src/components/OmMig.tsx
@@ -6,7 +6,28 @@ function OmMig() {
         <section  id="about-me-section" className="about-me" >
         <h2 id="who-am-i" className="title-main">Who am I?</h2>
         <div id="aboutme-wrapper">
-            <div id="text-box">
+            <div id="aboutme-box">
+                <p id="about-me-css-text">
+                    <span className="classtext">.about-me</span> <span className="normaltext">  &#123; <br></br>
+                    {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}name: </span> <span className="stringtext"> "Märta Ballardini"</span><span className="normaltext">;</span><br></br>
+                    <span className="normaltext">{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}age: </span> <span className="classtext"> 20</span><span className="normaltext">;</span><br></br>
+                    <span className="normaltext">{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}lives-in: </span> <span className="stringtext"> "Stockholm, Sweden"</span><span className="normaltext">;</span><br></br>
+                    <span className="normaltext">{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}likes-to: </span> <span className="stringtext"> “Hang out with friends and family”</span><span className="normaltext">; <br></br>
+                    &#125;</span>
+                    <br></br><br></br>
+                    <span className="idtext">#education</span> <span className="normaltext"> &#123;  <br></br>
+                    {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}school: </span> <span className="stringtext"> "Medieinstitutet"</span><span className="normaltext">;</span><br></br>
+                    <span className="normaltext">{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}program: </span> <span className="stringtext"> "Front End Developer"</span><span className="normaltext">;</span><br></br>
+                    <span className="normaltext">{'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}graduating: </span> <span className="stringtext"> "May 2022"</span><span className="normaltext">; <br></br>
+                    &#125;</span>
+
+                    <br></br><br></br>
+                    <span className="elementtext">cv</span> <span className="normaltext"> &#123;  <br></br>
+                    {'\u00A0'}{'\u00A0'}{'\u00A0'}{'\u00A0'}url: </span> <span className="elementtext"> <a href={cv} target="_blank" rel="noreferrer">$url_for_my_cv</a></span><span className="normaltext">;<br></br>
+                    &#125;</span>
+                </p>
+            </div>
+            {/* <div id="text-box">
                 <p>
                     My name is Märta Ballardini, i'm twenty years old and live in Stockholm, Sweden.
                     I am studying to become a Front End Developer, and i´m in my second year of the Front End Developer Program at Medieinstitutet which is a vocational school in Stockholm.
@@ -19,7 +40,7 @@ function OmMig() {
                 <p> Please have a look at my cv <a href={cv} target="_blank" rel="noreferrer">HERE!</a> </p>
                 
                 
-            </div>  
+            </div>   */}
             <img src={me} alt="" />
         </div> 
         
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,11 +1,142 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+interface skill {
+    name: string,
+    rating: number
+}
 function Skills() {
-  
+    const skills = {
+        languages: [
+            {
+                name: "HTML",
+                rating: 4
+            },
+            {
+                name: "CSS",
+                rating: 4
+            },
+            {
+                name: "JavaScript",
+                rating: 5
+            },
+            {
+                name: "Python",
+                rating: 2
+            },
+        ],
+        frameworks: [
+            {
+                name: "React JS",
+                rating: 4
+            },
+            {
+                name: "Angular",
+                rating: 3
+            },
+            {
+                name: "jQuery",
+                rating: 4
+            },
+            {
+                name: "Node JS",
+                rating: 3
+            },
+
+        ],
+        design: [
+            {
+                name: "Adobe XD",
+                rating: 4
+            },
+            {
+                name: "Figma",
+                rating: 3
+            },
+            {
+                name: "Photoshop",
+                rating: 3
+            },
+            {
+                name: "Illustrator",
+                rating: 3
+            }
+
+        ],
+        workflow:[
+            {
+                name: "Git/Github",
+                rating: 4
+            },
+            {
+                name: "Scrum",
+                rating: 3
+            },
+            {
+                name: "VS Code",
+                rating: 4
+            },
+
+        ]
+    }
+    const [navItem, setNavItem] = useState("languages")
+    const [skillsInfo, setSkillsInfo] = useState<any>()
+    let star = <i className="fas fa-star"></i>
+    function skillItemHtml (nav: skill[], title: string) {
+        setSkillsInfo(
+            <>
+            <h2>{title}</h2> 
+       { nav.map( (item) => (
+            
+            <div className="skill-item">
+                
+                <span>{item.name}</span> {'\u00A0'}
+                <div>
+                {[...Array(item.rating),
+                    ].map((value: undefined, index: number) => (
+                        <i className="fas fa-star"></i>
+                    ))}
+                </div>
+                
+                
+            </div>
+            
+        ))}
+        </>
+
+        )
+    }
+    function toggleinfo () {
+        if(navItem === "languages") {
+           skillItemHtml(skills.languages, "Languages")
+        } else if(navItem === "frameworks") {
+            skillItemHtml(skills.frameworks, "Frameworks & Libraries")
+        }else if(navItem === "design") {
+            skillItemHtml(skills.design, "Design")
+        }else if(navItem === "workflow") {
+            skillItemHtml(skills.workflow, "Workflow")
+        }
+    }
+    useEffect(() => {
+        toggleinfo();
+        
+    }, [navItem])
+    
     return (
         <section  id="skills-section" className="skills" >
             <h2 id="skills" className="title-main">My Skills</h2>
         <div className="skills-wrapper">
-            <div className="skills-container">
+            <div id="skills-box">
+                <div id="skills-nav">
+                    <div className="skills-nav-item" onClick={() => setNavItem("languages")}><span>Languages</span></div>
+                    <div className="skills-nav-item" onClick={() => setNavItem("frameworks")}><span>Frameworks & Libraries</span></div>
+                    <div className="skills-nav-item" onClick={() => setNavItem("design")}><span>Design</span></div>
+                    <div className="skills-nav-item" onClick={() => setNavItem("workflow")}><span>Workflow</span></div>
+                </div>
+                <div id="skills-info">
+                    {skillsInfo}
+                </div>
+            </div>
+
+            {/* <div className="skills-container">
                 <i style={{'color': '#e34f26'}} className="fab fa-html5"></i>
                 <i style={{'color': '#1472B6'}} className="fab fa-css3-alt"></i>
                 <i style={{'color': '#f7df1e'}} className="fab fa-js"></i>
@@ -15,7 +146,7 @@ function Skills() {
                 <i style={{'color': '#CE679B'}} className="fab fa-sass"></i>
                 <i style={{'color': '#4584b6'}} className="fab fa-python"></i>
                 <i style={{'color': '#F05133'}} className="fab fa-git-alt"></i>
-            </div>
+            </div> */}
         </div>
         </section>
     )
